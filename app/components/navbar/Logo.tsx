4048a3d6a7b0600f4324e955b80d51b9c439f382
PrimaSentana <premasentana123@gmail.com>
2024-01-20 16:36:57 +0800

    logo navbar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from 'next/navigation';
+
+const Logo = () => {
+   const router = useRouter();
+    
+   return (
+     <Image
+         width={120}
+         height={120}
+         src={"/logo.svg"}
+         alt="Roamradar"
+         className="hidden md:block cursor-pointer"
+		/>
+   )
+}
+
+export default Logo;

0537bbc3cf314886206ddb0b2c04aad908f68e68
PrimaSentana <premasentana123@gmail.com>
2024-01-28 15:06:58 +0800

    apply error notification on submit form

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,6 +9,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import toast from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -36,7 +38,7 @@ const RegisterModal = () => {
                 registerModal.onClose();
             })
             .catch((error) => {
-                console.log(error);
+                toast.error("Please input valid data")
             })
             .finally(() => {
                 setIsLoading(false);
@@ -74,6 +76,36 @@ const RegisterModal = () => {
         </div>
     )
 
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+                outline
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <Button
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <div className="flex flex-row justify-center items-center gap-2">
+                    <div>
+                        Already have account?
+                    </div>
+                    <div className="text-blue-400 font-semibold cursor-pointer hover:underline"
+                        onClick={registerModal.onClose}
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
     return (
         <Modal
             disabled={isLoading}
@@ -83,6 +115,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={footerContent}
         />
     )
 }

25cad1fe141454d16f2646039cbe36adacae7f30
PrimaSentana <premasentana123@gmail.com>
2024-02-09 13:31:20 +0800

    add src interface props to user profile

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -2,15 +2,18 @@
 
 import Image from "next/image";
 
+interface ProfileProps {
+	src: string | null | undefined
+}
 
-const Profile = () => {
+const Profile: React.FC<ProfileProps> = ({ src }) => {
   	return (
     	<Image
 			className="rounded-full"
 			height={30}
 			width={30}
 			alt="Profile Picture"
-			src={"/placeholder.jpg"}
+			src={src || "/placeholder.jpg"}
     	/>
   	)
 }

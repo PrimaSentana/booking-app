7f6cf33d05052004cf672081830c37967ad6622f
PrimaSentana <premasentana123@gmail.com>
2024-03-05 13:53:34 +0800

    set logic using watch from react-form to set (watch) the value in form.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -47,6 +47,7 @@ const RentModal = () => {
     })
 
     const category = watch("category");
+    const location = watch("location");
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -109,7 +110,10 @@ const RentModal = () => {
                     title="Where is your place located?"
                     subtitle="Help guests find it!"
                 />
-                <CountrySelect/>
+                <CountrySelect
+                    value={location}
+                    onChange={(value) => setCustomValue("location", value)}
+                />
             </div>
         )
     }

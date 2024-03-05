e475e58d677af6aba44142427064399cb19e13cc
PrimaSentana <premasentana123@gmail.com>
2024-03-05 14:09:23 +0800

    reassigned the icon image from leaflet source

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -10,6 +10,11 @@ import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src
+})
 
 const Map = () => {
   return (

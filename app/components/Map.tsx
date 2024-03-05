62a67ddcb4589792a46c8f6978164bd4babc4cd9
PrimaSentana <premasentana123@gmail.com>
2024-03-05 14:21:31 +0800

    center value [51, -0.09] is a placeholder for the default map position

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import L from "leaflet";
+import L, { LatLngBoundsExpression } from "leaflet";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css";
@@ -14,11 +14,19 @@ L.Icon.Default.mergeOptions({
     iconUrl: markerIcon.src,
     iconRetinaUrl: markerIcon2x.src,
     shadowUrl: markerShadow.src
-})
+});
 
-const Map = () => {
+interface MapProps {
+    center?: number[];
+}
+
+const Map: React.FC<MapProps> = ({ center }) => {
   return (
-    <div>Map</div>
+    <MapContainer
+        center={center as L.LatLngExpression || [51, -0.09]}
+    >
+
+    </MapContainer>
   )
 }
 

3e3c8449e9b69d4747966755488053474d06c251
PrimaSentana <premasentana123@gmail.com>
2024-03-24 21:31:22 +0800

    apply EmptyState component on page route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
 import Image from 'next/image'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState';
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <EmptyState showReset/>
+    )
+  }
+
   return (
-    <h1>Hello World</h1>
+    <Container>
+      <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+        <div className=''>
+          my listings
+        </div>
+      </div>
+    </Container>
   )
 }

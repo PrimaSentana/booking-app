68cf06e446643199fee1af2ddf7559052775d762
PrimaSentana <premasentana123@gmail.com>
2024-03-29 21:35:05 +0800

    add ListingCards components to show the listings on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
+import ListingCard from './components/listings/ListingCard';
+import getCurrentUser from './actions/getCurrentUser';
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <EmptyState showReset/>
     )
@@ -14,8 +18,16 @@ export default function Home() {
   return (
     <Container>
       <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <div className=''>
-          my listings
+        <div>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                data={listing}
+                currentUser={currentUser}
+              />
+            )
+          })}
         </div>
       </div>
     </Container>

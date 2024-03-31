ba71f8d4e7f6de43a905d7d29deb09082edd2119
PrimaSentana <premasentana123@gmail.com>
2024-03-31 21:04:59 +0800

    apply useFavorite hooks

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
     listingId: string;
@@ -9,8 +10,7 @@ interface HeartButtonProps {
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({ listingId, currentUser }) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {};
+    const { hasFavorited, toggleFavorite } = useFavorite({ currentUser, listingId });
 
     return (
         <div className="relative hover:opacity-80 transition cursor-pointer"
@@ -22,4 +22,4 @@ const HeartButton: React.FC<HeartButtonProps> = ({ listingId, currentUser }) => 
     )
 }
 
-export default HeartButton
+export default HeartButton;

4238591c9a88f20137e23c4c38c9a94da683ff1c
PrimaSentana <premasentana123@gmail.com>
2024-04-03 23:52:02 +0800

    fix little bug, adding square brackets for dynamic route

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -6,8 +6,8 @@ import prisma from "@/app/libs/prismadb";
 interface IParams {
     listingId?: string;
 }
-
-export async function POST(request: Request, { params }: { params: IParams}) {
+// { params } berbentuk destructuring object dengan tipe interface IParams
+export async function POST(request: Request, { params }: {params: IParams}) { 
     const currentUser = await getCurrentUser();
 
     if (!currentUser) {

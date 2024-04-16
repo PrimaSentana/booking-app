df5934c090ae920620898ff629d05d4fb53d05c2
PrimaSentana <premasentana123@gmail.com>
2024-04-16 13:59:41 +0800

    action that use to get listings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,8 +1,76 @@
 import prisma from '@/app/libs/prismadb';
 
-export default async function getListings() {
+
+export interface IListingsParams {
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+}
+
+export default async function getListings(params: IListingsParams) {
     try {
+        const { userId, guestCount, roomCount, bathroomCount, locationValue, startDate, endDate, category} = params;
+
+        let query: any = {}
+
+        if (userId) {
+            query.userId = userId
+        }
+
+        if (category) {
+            query.category = category
+        }
+
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate},
+                                startDate: { lte: startDate}
+                            },
+                            {
+                                startDate: { lte: endDate },
+                                endDate: { gte: endDate }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
+        if (userId) {
+            query.userId = userId
+        }
+
         const listings = prisma.listing.findMany({
+            where: query,
             orderBy: {
                 createdAt: 'desc'
             }

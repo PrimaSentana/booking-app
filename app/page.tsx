c61a5e09daa389130a165e5548b8af9502db2f14
PrimaSentana <premasentana123@gmail.com>
2024-04-16 13:59:18 +0800

    make params that can be use to filter listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
+
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
-import getListings from './actions/getListings';
+import getListings, { IListingsParams } from './actions/getListings';
 import ListingCard from './components/listings/ListingCard';
 import getCurrentUser from './actions/getCurrentUser';
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -14,7 +18,7 @@ export default async function Home() {
       <EmptyState showReset/>
     )
   }
-
+  
   return (
     <Container>
       <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
